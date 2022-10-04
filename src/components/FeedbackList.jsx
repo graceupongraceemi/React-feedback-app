38124e0259d9c138648e39a203d3fee94cb4ee8b
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-03 17:45:16 -0700

    installed react-icons (includes fontawesome & Material UI) and worked on deleteFeedback function in App.js (props drilling from FeedbackList and FeedbackItem components)

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
@@ -10,7 +10,12 @@ function FeedbackList({ feedback }) {
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          // handleDelete={(id) => console.log(id)}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
   );

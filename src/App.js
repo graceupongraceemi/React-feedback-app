34b903fc9f0a89cb9555a84acf1cd3f4091204ab
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-04 15:59:53 -0700

    RatingSelect.jsx component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);

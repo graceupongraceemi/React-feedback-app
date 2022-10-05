44b7e9971a3351fb1ee647851471881ea9ca9074
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-04 17:27:21 -0700

    Installed framer-motion into FeedbackList component for fade out and in effect for feedbacks

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
@@ -9,17 +10,40 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          // handleDelete={(id) => console.log(id)}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              // handleDelete={(id) => console.log(id)}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
+
+//   return (
+//     <div className='feedback-list'>
+//       {feedback.map((item) => (
+//         <FeedbackItem
+//           key={item.id}
+//           item={item}
+//           // handleDelete={(id) => console.log(id)}
+//           handleDelete={handleDelete}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
